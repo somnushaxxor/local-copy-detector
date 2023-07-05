4b9d3e4cc2e6cc96ab87d86ecfe727a5ab39ab76
Artem Kolesnik <steeltouch27@gmail.com>
2023-07-05 17:18:49 +0600

    Add GroupMessageDeserializationException

---
+++ src/main/java/ru/nsu/fit/kolesnik/localcopydetector/message/GroupMessageDeserializationException.java
@@ -0,0 +1,9 @@
+package ru.nsu.fit.kolesnik.localcopydetector.message;
+
+public class GroupMessageDeserializationException extends RuntimeException {
+
+    public GroupMessageDeserializationException(Throwable cause) {
+        super(cause);
+    }
+
+}
